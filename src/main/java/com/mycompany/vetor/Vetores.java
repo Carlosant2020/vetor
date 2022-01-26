e90c3870d32ec39a31ac763d1aa25780be282481
Carlos Antonio <carlosantoniodelfino@gmail.com>
2022-01-26 00:17:00 -0300

    first commit

---
+++ src/main/java/com/mycompany/vetor/Vetores.java
@@ -0,0 +1,20 @@
+
+package com.mycompany.vetor;
+
+/**
+ *
+ * @author carlo
+ */
+public class Vetores {
+
+   
+    public static void main(String[] args) {
+        int n[] = {3,2,8,7,5,4};
+        
+        for(int c=0; c<=5; c++){
+            System.out.print("Na posicao " + c +"Temos o valor " + n[c]);
+        }
+        
+    }
+    
+}
